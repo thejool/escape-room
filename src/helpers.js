17acc64eca38cec15c4f18bd486c5a32ac79bac9
Joel Gustafsson <joel.gustafsson@knowit.se>
2020-02-12 15:16:30 +0100

    Changed fizzbuzz

--- src/helpers.js
+++ src/helpers.js
@@ -81,7 +81,7 @@ export const fizzBuzz = () => {
 
   for(let i = 1; i < 101; i++) {
     if(i % 15 === 0) {
-      res.push('Fizz Buzz')
+      res.push('FizzBuzz')
     } else if(i % 5 === 0) {
       res.push('Buzz')
     } else if(i % 3 === 0) {
